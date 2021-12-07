04e4d57a5bfb3a580b52235addfabf8238d2af32
Максим Реснянский <mresnyansky@gmail.com>
2021-12-07 16:18:29 +0200

    Add project files.

---
+++ 2.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число y:");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число z:");
+            int z = int.Parse(Console.ReadLine());
+            if (y > x)
+            {
+                x = y;
+            }
+            else
+            {
+
+            }
+            if (z > x)
+            {
+                x = z;
+            }
+            else
+            {
+
+            }
+            Console.WriteLine(x);
+        }
+
+
+    }
+}
